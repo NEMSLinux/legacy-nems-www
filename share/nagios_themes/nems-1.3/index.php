2e8ca47374e201c5b4f1650dc7acb6e88d2e7288
Cat5TV <contact@category5.tv>
2018-05-19 23:49:21 -0400

    Replace accidentally reverted Nagios theme

--- share/nagios_themes/nems-1.3/index.php
+++ share/nagios_themes/nems-1.3/index.php
@@ -1,62 +1,25 @@
-<?php
-// Allow specifying main window URL for permalinks, etc.
-$url = 'main.php';
-
-if ("no" == "yes" && isset($_GET['corewindow'])) {
-
-	// The default window url may have been overridden with a permalink...
-	// Parse the URL and remove permalink option from base.
-	$a = parse_url($_GET['corewindow']);
-
-	// Build the base url.
-	$url = htmlentities($a['path']).'?';
-	$url = (isset($a['host'])) ? $a['scheme'].'://'.$a['host'].$url : '/'.$url;
-
-	$query = isset($a['query']) ? $a['query'] : '';
-	$pairs = explode('&', $query);
-	foreach ($pairs as $pair) {
-		$v = explode('=', $pair);
-		if (is_array($v)) {
-			$key = urlencode($v[0]);
-			$val = urlencode(isset($v[1]) ? $v[1] : '');
-			$url .= "&$key=$val";
-		}
-	}
-	if (preg_match("/^http:\/\/|^https:\/\/|^\//", $url) != 1)
-		$url = "main.php";
-}
-
-$this_year = '2017';
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
-
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
-<script LANGUAGE="javascript">
-	var n = Math.round(Math.random() * 10000000000);
-	document.write("<title>Nagios Core on " + window.location.hostname + "</title>");
-	document.cookie = "NagFormId=" + n.toString(16);
-</script>
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/ico">
+	<title>NEMS - Nagios Core</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta http-equiv="Content-Language" content="en" />
+	<meta name="robots" content="noindex, nofollow" />
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
 </head>
-
-<frameset cols="180,*" style="border: 0px;">
-	<frame src="side.php" name="side" frameborder="0" style="">
-	<frame src="<?php echo $url; ?>" name="main" frameborder="0" style="">
-
+	<frameset frameborder="0" framespacing="0" rows="60,*">
+		<frame src="top.php" name="top" />
+		<frameset frameborder="0" framespacing="0" cols="200,*">
+			<frame src="menu.html" name="menu" target="main" />
+			<frameset frameborder="0" framespacing="0" rows="26,*">
+				<frame src="sidebar.html" name="navigation" />
+				<frame src="cgi-bin/tac.cgi" name="main" />
+			</frameset>
+		</frameset>
+	</frameset>
 	<noframes>
-		<!-- This page requires a web browser which supports frames. -->
-		<h2>Nagios Core</h2>
-		<p align="center">
-			<a href="https://www.nagios.org/">www.nagios.org</a><br>
-			Copyright &copy; 2010-<?php echo $this_year; ?> Nagios Core Development Team and Community Contributors.
-			Copyright &copy; 1999-2010 Ethan Galstad<br>
-		</p>
-		<p>
-			<i>Note: These pages require a browser which supports frames</i>
-		</p>
+		<body>
+			<p>These pages require a browser which supports frames.</p>
+		</body>
 	</noframes>
-</frameset>
-
 </html>
