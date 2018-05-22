51d939e6489772f26966b44888dc017fa5dcb42f
Robbie Ferguson <contact@category5.tv>
2018-05-21 22:48:39 -0400

    Remove Check_MK and add Adagios to NEMS 1.4

--- inc/header.php
+++ inc/header.php
@@ -121,7 +121,8 @@ The data-spy and data-target are part of the built-in Bootstrap scrollspy functi
 							    }
 							  ?>
 							  <li><a href="/nagvis/" target="_blank">NagVis</a></li>
-							  <li><a href="/check_mk/" target="_blank">Check_MK Multisite</a></li>
+							  <?php if (ver('nems') <= 1.3.1) echo '<li><a href="/check_mk/" target="_blank">Check_MK Multisite</a></li>'; ?>
+							  <?php if (ver('nems') >= 1.4) echo '<li><a href="/adagios/" target="_blank">Adagios</a></li>'; ?>
 							  <?php if (ver('nems') >= 1.4) echo '<li><a href="/mobile/" target="_blank">NEMS Mobile UI</a></li>'; ?>
 							</ul>
 						</li>
