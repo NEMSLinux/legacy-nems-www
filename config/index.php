03380cf8c12cbddc25622606f7d33c7883ff233e
NEMS Linux <nems@baldnerd.com>
2018-05-26 20:03:59 +0000

    Make it so NEMS SST restarts Nagios service after changing settings

--- config/index.php
+++ config/index.php
@@ -67,6 +67,9 @@ if (isset($_POST) && isset($_POST['email'])) {
   $output .= '$USER14$=' . (sanitize($_POST['pushover_userkey']) ?: 'NULL') . PHP_EOL;
 
   file_put_contents($resourcefile,$output); // overwrite the existing config
+
+  # Restart Nagios
+  $response = shell_exec('sudo /bin/systemctl restart nagios');
 }
 
 $resource = file($resourcefile);
@@ -130,6 +133,12 @@ function sanitize($string) {
 <div class="container" style="margin-top: 100px; padding-bottom: 100px;">
   <h2><b>NEMS</b> <b>S</b>ystem <b>S</b>ettings <b>T</b>ool</h2>
   <p style="padding:4px 6px; color: #aaa !important;"><b>Your NEMS Hardware ID:</b> <span class="nems-green"><?= shell_exec('/usr/local/bin/nems-info hwid'); ?></span></p>
+
+
+<?php
+  if (strlen($response) > 0) echo '<div class="col-md-12 alert alert-danger fade in">' . $response . '</div>';
+?>
+
 
 <form method="post" id="sky-form4" class="sky-form">
 
