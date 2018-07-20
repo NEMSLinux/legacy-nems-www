5fa57ec88882df090f5bf5aafcc2fde04917a891
Robbie Ferguson <contact@category5.tv>
2018-07-20 13:12:03 -0400

    Make time in 12 hour format

--- tv/index.php
+++ tv/index.php
@@ -46,6 +46,18 @@
             
             
             // timestamp stuff
+		
+		function tConvert (time) {
+		  // Check correct time format and split into components
+		  time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+		  if (time.length > 1) { // If time format correct
+		    time = time.slice (1);  // Remove full string match value
+		    time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+		    time[0] = +time[0] % 12 || 12; // Adjust hours
+		  }
+		  return time.join (''); // return adjusted time or original string
+		}
             
             function createTimeStamp() {
                 // create timestamp
@@ -53,7 +65,7 @@
                 ts = ts.toTimeString();
                 ts = ts.replace(/\s+GMT.+/ig, "");
 //                ts = ts.replace(/\:\d+(?=$)/ig, "");
-                $("#timestamp_wrap").empty().append("<div class=\"timestamp_drop\"></div><div class=\"timestamp_stamp\">" + ts +"</div>");
+                $("#timestamp_wrap").empty().append("<div class=\"timestamp_drop\"></div><div class=\"timestamp_stamp\">" + tConvert(ts) +"</div>");
             }
             
             function updateNagiosData(block){
