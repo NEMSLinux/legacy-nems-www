949c5043c26b3d6c8c705fc7e21913593b680618
NEMS Linux <nems@baldnerd.com>
2018-08-17 08:08:27 -0400

    Move and activate phoronix front-end

---
+++ phoronix/index.php
@@ -0,0 +1,15 @@
+<?php
+  echo '<h1>Phoronix Test Suite for NEMS Linux</h1><h2>Weekly Benchmarks</h2>';
+
+  $logs = array_diff(scandir('/var/log/nems/phoronix/'), array('..', '.', 'index.php'));
+  if (is_array($logs) && count($logs) > 0) {
+    rsort($logs);
+    echo '<ul>' . PHP_EOL;
+    foreach ($logs as $log) {
+      echo '  <li><a href="./' . $log . '">' . date('F j, Y',strtotime($log)) . '</a></li>' . PHP_EOL;
+    }
+    echo '</ul>' . PHP_EOL;
+  } else {
+    echo 'Your NEMS Linux server is too new.<br />Benchmarks will be generated within 7 days (assuming you leave it running 24/7).<br />Please check back soon.';
+  }
+?>
