dce89e0b7f93286673c9c158c3a418a95184b274
NEMS Linux <nems@baldnerd.com>
2018-12-31 12:15:45 -0800

    Fix Scheduled Downtime in NEMS Mobile UI

--- mobile/include.inc.php
+++ mobile/include.inc.php
@@ -11,7 +11,7 @@ $STATUS_FILE  = "/var/cache/nagios/status.dat";
 $COMMAND_FILE = "/usr/local/nagios/var/rw/nagios.cmd";
 $CGI_FILE = "/usr/local/nagios/etc/cgi.cfg";
 $OBJECTS_FILE = "/var/lib/nagios/objects.precache"; 
-$BASE_URL = '/nagiosmobile';
+$BASE_URL = '/mobile';
 $TTL = 30; //Time to live for cached data parsed from status and objects file 
 $USER = ''; //you can hard-code the 'nagiosadmin' if you don't want any authentication: NOT RECOMMENDED!! 
 ///////////////////////////////////////////////////////////////////////////////////
