5f2ab96da9bed8f2654093973b94d62447c041da
NEMS Linux <nems@baldnerd.com>
2019-02-25 16:42:28 -0500

    Change mobile browser color

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+  $bgcolor = '040111';
+?><!DOCTYPE html>
 <!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
 <!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
 <!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
@@ -21,6 +23,7 @@
 
 	<!-- Favicon -->
         <link rel="shortcut icon" href="/favicon.ico">
+        <meta name="theme-color" content="#<?= $bgcolor ?>">
 
 	<!-- Web Fonts -->
 	<link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600&amp;subset=cyrillic,latin'>
