cfbb6fdf52f3c83db3ec6c2ca0aa094870453077
Cat5TV <contact@category5.tv>
2020-05-28 20:46:04 -0400

    Credits: Add ogg to audio

--- credits/index.php
+++ credits/index.php
@@ -151,6 +151,7 @@ window.findKeyframesRule = function (rule) {
   <!-- The audio is streaming from Robbie's server and does not reside on your NEMS Server. -->
   <!-- While licensed for this project, you may not use the music elsewhere. -->
   <audio autoplay="autoplay" id="music">
+    <source src="https://cdn.zecheriah.com/nems/audio/Movie-Score-Jeffrey-Peterson.ogg" type="audio/ogg">
     <source src="https://cdn.zecheriah.com/nems/audio/Movie-Score-Jeffrey-Peterson.mp3" type="audio/mpeg">
   </audio>
 
