65e3810dce9740e58796e5bd27afdad9d005774c
NEMS Linux <nems@baldnerd.com>
2020-12-10 11:40:58 -0500

    check_nems_php_agent version 1.2

--- config/php-agent-gen.php
+++ config/php-agent-gen.php
@@ -12,7 +12,7 @@ if (is_array($conf)) { // Load the existing conf data
 if ($php_agent_key == '') die('Missing passphrase. Did you set one in NEMS SST?');
 
 $nemsver = shell_exec('/usr/local/bin/nems-info nemsver');
-$nemsagentver = '1.1';
+$nemsagentver = '1.2';
 
 $data = '<' . '?php
   // This is the NEMS PHP Server Agent v' . $nemsagentver . '
@@ -24,9 +24,12 @@ $data = '<' . '?php
   // You can either edit it here base64 encoded, or just download the new agent (recommended) and re-upload to your server
   $encryptionkey = \'' . openssl_encrypt($php_agent_key,"AES-128-ECB",base64_encode(':' . $php_agent_key . ':')) . '\';
 
+  $check = filter_var($_POST[\'check\'], FILTER_SANITIZE_STRING);
+
   $data = array();
 
   // CPU Data
+  if ($check == \'load\') {
     $prevVal = shell_exec("cat /proc/stat");
     $prevArr = explode(\' \',trim($prevVal));
     $prevTotal = $prevArr[2] + $prevArr[3] + $prevArr[4] + $prevArr[5];
@@ -45,16 +48,20 @@ $data = '<' . '?php
     $data[\'cpu\'][\'loadaverage\'][1] = $tmp[0];
     $data[\'cpu\'][\'loadaverage\'][5] = $tmp[1];
     $data[\'cpu\'][\'loadaverage\'][15] = $tmp[2];
+  }
 
   //memory stat
+  if ($check == \'mem\') {
     $data[\'mem\'][\'percent\'] = round(shell_exec("free | grep Mem | awk \'{print $3/$2 * 100.0}\'"), 2);
     $mem_result = shell_exec("cat /proc/meminfo | grep MemTotal");
     $data[\'mem\'][\'total\'] = round(preg_replace("#[^0-9]+(?:\.[0-9]*)?#", "", $mem_result) / 1024 / 1024, 3);
     $mem_result = shell_exec("cat /proc/meminfo | grep MemFree");
     $data[\'mem\'][\'free\'] = round(preg_replace("#[^0-9]+(?:\.[0-9]*)?#", "", $mem_result) / 1024 / 1024, 3);
     $data[\'mem\'][\'used\'] = $data[\'mem\'][\'total\'] - $data[\'mem\'][\'free\'];
+  }
 
   //hdd stat
+  if ($check == \'.\') {
     $mountpoint = \'.\';
     $df = disk_free_space($mountpoint);
     $dt = disk_total_space($mountpoint);
@@ -67,7 +74,9 @@ $data = '<' . '?php
       $data[\'storage\'][$mountpoint][\'used\'] = $data[\'storage\'][$mountpoint][\'total\'] - $data[\'storage\'][$mountpoint][\'free\'];
       $data[\'storage\'][$mountpoint][\'percent\'] = round(sprintf(\'%.2f\',($data[\'storage\'][$mountpoint][\'used\'] / $data[\'storage\'][$mountpoint][\'total\']) * 100), 2);
     }
+  }
 
+  if ($check == \'/\') {
     $mountpoint = \'/\';
     $df = disk_free_space($mountpoint);
     $dt = disk_total_space($mountpoint);
@@ -79,7 +88,9 @@ $data = '<' . '?php
       $data[\'storage\'][$mountpoint][\'used\'] = $data[\'storage\'][$mountpoint][\'total\'] - $data[\'storage\'][$mountpoint][\'free\'];
       $data[\'storage\'][$mountpoint][\'percent\'] = round(sprintf(\'%.2f\',($data[\'storage\'][$mountpoint][\'used\'] / $data[\'storage\'][$mountpoint][\'total\']) * 100), 2);
     }
+  }
 
+  if ($check == \'var\') {
     $mountpoint = \'/var\';
     $df = disk_free_space($mountpoint);
     $dt = disk_total_space($mountpoint);
@@ -91,10 +102,37 @@ $data = '<' . '?php
       $data[\'storage\'][$mountpoint][\'used\'] = $data[\'storage\'][$mountpoint][\'total\'] - $data[\'storage\'][$mountpoint][\'free\'];
       $data[\'storage\'][$mountpoint][\'percent\'] = round(sprintf(\'%.2f\',($data[\'storage\'][$mountpoint][\'used\'] / $data[\'storage\'][$mountpoint][\'total\']) * 100), 2);
     }
+  }
 
   //network stat
-    $data[\'network\'][\'rx\'] = round(trim(file_get_contents("/sys/class/net/eth0/statistics/rx_bytes")) / 1024/ 1024/ 1024, 2);
-    $data[\'network\'][\'tx\'] = round(trim(file_get_contents("/sys/class/net/eth0/statistics/tx_bytes")) / 1024/ 1024/ 1024, 2);
+  if ($check == \'net\' || $check == \'netrx\' || $check == \'nettx\') {
+    $ifstat = trim(shell_exec(\'which ifstat\'));
+    if (strlen($ifstat) > 0) {
+      // Obtain output from ifstat
+      $seconds = 5; // how many seconds to average (affects how long the check takes)
+      $network = shell_exec("$ifstat -T $seconds 1");
+      // Convert ifstat output to an array
+      $networkARR = array_values(array_filter(explode(PHP_EOL,$network)));
+      // Trim the array values
+      foreach ($networkARR as $key => $line) {
+        $networkARR[$key] = trim($line);
+      }
+      // Parse it into a clean array
+      // array_values resets the key to sequential after array_filter removes empty elements
+      $interfaces = array_values(array_filter(explode(\'  \',$networkARR[0])));
+      $headings = array_values(array_filter(explode(\'  \',$networkARR[1])));
+      $values = array_values(array_filter(explode(\'  \',$networkARR[2])));
+      $counter=0;
+      // 0.008 multiplier to convert the KB/s output to Mb/s
+      foreach ($interfaces as $key => $intname) {
+        $data[\'network\'][strtolower(trim($intname))][\'rx\'] = round((trim($values[$counter++])*0.008),2);
+        $data[\'network\'][strtolower(trim($intname))][\'tx\'] = round((trim($values[$counter++])*0.008),2);
+      }
+    } else {
+      $data[\'network\'][\'none\'][\'rx\'] = 0;
+      $data[\'network\'][\'none\'][\'tx\'] = 0;
+    }
+  }
 
   //output headers
     header(\'Content-type: text/json\');
@@ -118,4 +156,9 @@ header('Content-Type: text/plain');
 header('Content-Length: ' . strlen($data));
 header('Connection: close');
 echo $data;
+
+// Could iterate through the /sys/class/net folder if ifstat isn't installed... does not seem as accurate though.
+//    $data[\'network\'][\'rx\'] = round(trim(file_get_contents("/sys/class/net/eth0/statistics/rx_bytes")) / 1024/ 1024/ 1024, 2);
+//    $data[\'network\'][\'tx\'] = round(trim(file_get_contents("/sys/class/net/eth0/statistics/tx_bytes")) / 1024/ 1024/ 1024, 2);
+
 ?>
